2a1a86f0f7c516399b77da6ddf3b6e79ecbcbb12
Koichi Osanai <osanai3@gmail.com>
2024-05-18 17:27:47 +0900

    parse csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{error::Error, io, process};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = csv2jsonl() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn csv2jsonl() -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_reader(io::stdin());
+    let headers = reader.headers()?.to_owned();
+    for result in reader.records() {
+        let record = result?;
+        for (header, field) in headers.iter().zip(record.iter()) {
+            println!("{} {}", header, field);
+        }
+    }
+    Ok(())
 }
