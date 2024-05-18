f69c6827f556526a19619295e4803e455910411e
Koichi Osanai <osanai3@gmail.com>
2024-05-18 17:43:05 +0900

    colect to hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{error::Error, io, process};
+use csv::Reader;
+use std::{collections::HashMap, error::Error, io, process};
 
 fn main() {
     if let Err(err) = csv2jsonl() {
@@ -8,13 +9,15 @@ fn main() {
 }
 
 fn csv2jsonl() -> Result<(), Box<dyn Error>> {
-    let mut reader = csv::Reader::from_reader(io::stdin());
+    let mut reader = Reader::from_reader(io::stdin());
     let headers = reader.headers()?.to_owned();
     for result in reader.records() {
+        let mut map = HashMap::<&str, &str>::new();
         let record = result?;
         for (header, field) in headers.iter().zip(record.iter()) {
-            println!("{} {}", header, field);
+            map.insert(header, field);
         }
+        println!("{:?}", map);
     }
     Ok(())
 }
